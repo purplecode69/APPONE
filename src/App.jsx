d1fff156f59f5a19ef08b4933e70212ca9d3ef65
shah Paran <sq.poran@gmail.com>
2026-01-20 07:22:23 -0500

    thsi is ongoing practice part

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
 
 const App = () => {
-  let  status = true;
+  let status = false;
 
   return (
     <div>
-      <h1>Login Status </h1>
-      {status && <button>LogOut</button>}
+      <h1>Login status</h1>
+      {
+       (()=>{
+        if(status== true){
+          return(
+            <button>Logout</button>
+          )
+        }
+        else{
+          return(
+            <button>Login</button>
+          )
+        }
 
-      
+
+       }) ()
+      }
     </div>
   );
 };
