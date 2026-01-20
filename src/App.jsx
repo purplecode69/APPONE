e6feba02dc4db6413b7a58921a9005999b5acb05
shah Paran <sq.poran@gmail.com>
2026-01-20 06:20:26 -0500

    practice is on going

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,13 @@
 import React from 'react';
 
-const loginStatus = (status)=>{
-  if (status){
-    return (
-     <div>
-      <h1>akhon login kora acha </h1>
-      <button>Logout</button>
-     </div>
-    )
-
-  }else{
-    return(
-      <div>
-        <h1>Akhon LOgout kora acha </h1>
-        <button>Please Login </button>
-      </div>
-    )
-  }
-}
-
 const App = () => {
+  let  status = true;
+
   return (
     <div>
-      {loginStatus(true)}
+      <h1>Login Status </h1>
+      {status && <button>LogOut</button>}
+
       
     </div>
   );
